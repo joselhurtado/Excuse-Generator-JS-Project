7fe9043c77d5e6a84b3f2c033f0777cacd5d677c
Jose Hurtado <hello@hurtadojose.com>
2022-05-25 00:03:41 +0000

    remove assets

--- src/main.js
+++ src/main.js
@@ -2,9 +2,6 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = () => {
   document.querySelector("#excuse").innerHTML = generateExcuse();
 };
